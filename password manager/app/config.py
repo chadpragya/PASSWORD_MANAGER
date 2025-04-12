134f6561f128a4302ff28be4a100b6f756e2cd6e
chadpragya <140604547+chadpragya@users.noreply.github.com>
2025-04-12 19:46:40 +0530

    Delete password manager directory

--- password manager/app/config.py
+++
@@ -1,6 +0,0 @@
-import os
-from datetime import timedelta
-
-class Config:
-    JWT_SECRET_KEY = os.environ.get('JWT_SECRET_KEY', 'dev-jwt-secret-key')
-    JWT_ACCESS_TOKEN_EXPIRES = timedelta(hours=1)
